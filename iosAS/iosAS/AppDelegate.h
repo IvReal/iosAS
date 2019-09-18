d0d84c7918f06cf0dbd3afffa40902346d7f802f
IvReal <ivreal@yandex.ru>
2019-09-18 14:35:29 +0300

    Initial project

---
+++ iosAS/iosAS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iosAS
+//
+//  Created by Iv on 18/09/2019.
+//  Copyright © 2019 Iv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iosAS/iosAS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosAS
+//
+//  Created by Iv on 18/09/2019.
+//  Copyright © 2019 Iv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
